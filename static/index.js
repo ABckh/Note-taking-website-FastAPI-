0a9d5ca6d30e80c953111c948997c5cbb876ca3e
ABckh <abehodau@gmail.com>
2022-01-11 00:28:45 +0300

    Delete static file

--- static/index.js
+++
@@ -1,8 +0,0 @@
-function deleteNote(noteId){
-    fetch("/del", {
-        method: "POST",
-        body: JSON.stringify({noteId: noteId}),
-    }).then((_res) => {
-        window.location.href = "/";
-    });
-}
